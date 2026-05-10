365f35adc4d799b2ea36c52f6bbd5d0fd1f23856
metanoeitaii <dhmhtrhspapandreou@gmail.com>
2026-05-10 03:56:21 +0300

    Local testing IPs

--- src/main/java/master/ClientHandler.java
+++ src/main/java/master/ClientHandler.java
@@ -95,7 +95,7 @@ public class ClientHandler extends Thread {
                 String minStars = in.readLine();
 
                 //syndeomai me reducer opws MAP 
-                Socket reducerSocket = new Socket("172.20.10.7",4000);
+                Socket reducerSocket = new Socket("127.0.0.1",4000);
                 ObjectOutputStream reducerOut = new ObjectOutputStream(reducerSocket.getOutputStream());
                 reducerOut.writeObject(workerHosts.size());
                 reducerOut.writeObject("SEARCH"); 
@@ -146,7 +146,7 @@ public class ClientHandler extends Thread {
                 System.out.println("H entoli pou elava einai MAP");
                 String typosMap = in.readLine();
                 //sunedomai me reducer 
-                Socket reducerSocket = new Socket("172.20.10.7",4000);
+                Socket reducerSocket = new Socket("127.0.0.1",4000);
                 ObjectOutputStream reducerout = new ObjectOutputStream(reducerSocket.getOutputStream());
                 reducerout.writeObject(workerHosts.size());
                 reducerout.writeObject(Message.MAP);
